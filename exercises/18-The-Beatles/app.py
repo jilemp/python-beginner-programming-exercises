11f2506531b143a9fe3de068e7b26584e39a2210
Jose Ibarra <46697312+jilemp@users.noreply.github.com>
2024-10-03 09:27:26 +0000

    Submission 03/10/2024

--- exercises/18-The-Beatles/app.py
+++ exercises/18-The-Beatles/app.py
@@ -1,2 +1,13 @@
 # ✅↓ Write your code here ↓✅
 
+def sing():
+    lyrics = ''
+    for i in range(0,11):
+        if i == 4:
+            lyrics += 'there will be an answer,\n'
+        elif i == 10:
+            lyrics += 'whisper words of wisdom, let it be'
+        else:
+            lyrics += 'let it be,\n'
+    return lyrics
+sing()
--- exercises/19-Bottles-Of-Milk/app.py
+++ exercises/19-Bottles-Of-Milk/app.py
@@ -1,1 +1,15 @@
 # ✅↓ Write your code here ↓✅
+def number_of_bottles():
+    for i in range(99,-1, -1):
+        lyrics = ''
+        if i == 2:
+            lyrics += '2 bottles of milk on the wall, 2 bottles of milk. Take one down and pass it around, 1 bottle of milk on the wall.'
+        elif i == 1:
+            lyrics += '1 bottle of milk on the wall, 1 bottle of milk. Take one down and pass it around, no more bottles of milk on the wall.'
+        elif i == 0 :
+            lyrics += 'No more bottles of milk on the wall, no more bottles of milk. Go to the store and buy some more, 99 bottles of milk on the wall.'
+        else:
+            lyrics += str(i) + ' bottles of milk on the wall, ' + str(i) + ' bottles of milk. Take one down and pass it around, ' + str(i-1) + ' bottles of milk on the wall.'
+        print(lyrics)
+
+number_of_bottles()
